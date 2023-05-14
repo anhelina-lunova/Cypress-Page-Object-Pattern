aab7078c60a8447564a2ff96e5abaa8d9108514f
Anhelina Lunova <120516170+anhelina-lunova@users.noreply.github.com>
2023-05-14 21:30:06 +0300

    delete comments

--- cypress/e2e/pageObjPattern.cy.js
+++ cypress/e2e/pageObjPattern.cy.js
@@ -6,21 +6,12 @@ const homePage = new HomePage();
 
 describe("Module 3", () => {
   it("Тест №1", () => {
-    // cy.LogIn("user888@gmail.com", "1234567890");
-
-    // cy.get("#open-navigation-menu-mobile").click();
-    // cy.get(":nth-child(9) > .css-bve2vl").click();
     loginPage.navigateToPage();
     loginPage.inputUserDataAndLogIn("user888@gmail.com", "1234567890");
     homePage.logOut();
   });
 
   it("Тест №2", () => {
-    // cy.LogIn("testowyqa@qa.team", "QA!automation-1");
-
-    // cy.get("#open-navigation-menu-mobile").click();
-    // cy.get(":nth-child(7) > .css-bve2vl").click();
-
     loginPage.navigateToPage();
     loginPage.inputUserDataAndLogIn("testowyqa@qa.team", "QA!automation-1");
     homePage.logOut();
